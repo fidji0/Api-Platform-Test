23eedc05b69900f6218ac2514913b8d8c50cc89c
anthony <a.vincent.anthony@gmail.com>
2022-09-09 18:03:29 +0200

    maj

--- public/index.php
+++ public/index.php
@@ -1,7 +1,12 @@
 <?php
 
 use App\Kernel;
-
+header('Access-Control-Allow-Origin: https://localhost:4200');
+header("Access-Control-Allow-Headers: Authorization , X-AUTH-TOKEN , Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Allow: GET, POST, PUT, DELETE");
+header("Access-Control-Allow-Credentials: true ");
+$method = $_SERVER['REQUEST_METHOD'];
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
